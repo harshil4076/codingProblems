c097187b68ab921cc9441a8ab528b92e04ec4402
Harshil Patel <harshil4076@gmail.com>
2021-04-29 12:07:53 -0400

    Sorted

--- arraySort.js
+++ arraySort.js
@@ -1,7 +1,23 @@
 
-let arr = [2,1,0,2,0,1,0,2]
+let arr = [2,1,0,2,0,1,0,2,0,2,1,1,2,0,1]
+//////////
+let arr0 = []
+  let arr1 = []
+   let arr2 = []
 function arraySort (){
-    console.log(arr)
+    for(let i = 0; i<= arr.length-1; i++){
+        if(arr[i] == 0){
+            arr0.push(arr[i])
+        }
+        else if(arr[i] == 1){
+            arr1.push(arr[i])
+        }
+        else if(arr[i] == 2){
+            arr2.push(arr[i])
+        }
+        
+    }
+    return [...arr0, ...arr1, ...arr2]
 }
 
-arraySort()
+console.log(arraySort())
