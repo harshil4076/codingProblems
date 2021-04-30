4a725ba9bdcb05974ec37d003e42886df1374dfa
Harshil Patel <harshil4076@gmail.com>
2021-04-30 10:25:28 -0400

    negative on left positive on right

--- arraySort.js
+++ arraySort.js
@@ -4,6 +4,7 @@ let arr = [2,1,0,2,0,1,0,2,0,2,1,1,2,0,1]
 let arr0 = []
   let arr1 = []
    let arr2 = []
+   
 function arraySort (){
     for(let i = 0; i<= arr.length-1; i++){
         if(arr[i] == 0){
@@ -20,4 +21,40 @@ function arraySort (){
     return [...arr0, ...arr1, ...arr2]
 }
 
-console.log(arraySort())
+// An array contains both positive and negative numbers in random order. Rearrange the array elements so that all negative numbers appear before all positive numbers.
+// Examples : 
+
+// Input: -12, 11, -13, -5, 6, -7, 5, -3, -6
+// Output: -12 -13 -5 -7 -3 -6 11 6 5
+// Note: Order of elements is not important here.
+function negativeSort (input) {
+    // let negative =[]
+    // let positive = []
+
+    // for(i=0; i< input.length; i++){
+
+    //     if(Math.sign(input[i]) == -1){
+    //         negative.push(input[i])
+    //     }
+    //     else if(Math.sign(input[i]) == 1){
+    //         positive.push(input[i])
+    //     }
+    // }
+
+    // return [...negative, ...positive]
+    let res = input
+    let j = 0;
+    let temp;
+
+    for(i=0; i< res.length; i++){
+        if(res[i] < 0){
+            temp = res [i]  //-13
+            res[i] = res[j]  //res[i] = 2 = res[j] = 1
+            res[j] = temp
+            j++
+        }
+    }
+    return res
+}
+
+console.log(negativeSort([-12, 11, -13, -5, 6, -7, 5, -3, -6]))
